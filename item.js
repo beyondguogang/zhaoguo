4a57d611b6c283f4d4342fb108a4b15f05e94ecc
guogang <guogang1201@163.com>
2020-05-12 18:42:27 +0800

    222

--- item.js
+++ item.js
@@ -13,43 +13,52 @@ let vm=new Vue({
 }
         },*/
         data:{
-           s:'hahhah',
+            msg:'',
             isActive: false,
             isItemList:false,
             isItemList1:false,
-            isItemList2:false
+            isItemList2:false,
+            isIcon:false
         },
         methods:{
             fn(){
-                    this.isActive=true;
+                this.isActive=true;
            },
-
             fn2(){
                 this.isActive=false
            },
             fn3(){
-                if(!this.isItemList)
-                {
+                if(!this.isItemList) {
                 this.isItemList=true
                 }else{
                     this.isItemList=false
                 }
             },
             fn4(){
-                if(!this.isItemList1)
-                {
+                if(!this.isItemList1) {
                 this.isItemList1=true
                 }else{
                     this.isItemList1=false
                 }
             },
             fn5(){
-                if(!this.isItemList2)
-                {
+                if(!this.isItemList2) {
                 this.isItemList2=true
                 }else{
                     this.isItemList2=false
                 }
+
+            },
+            delIcon(){
+                this.isIcon=true
+                if(this.msg===''){
+                    this.isIcon=false
+                }
+
+            },
+            ico(){
+                this.isIcon=false;
+                this.msg='';
             }
 
 
